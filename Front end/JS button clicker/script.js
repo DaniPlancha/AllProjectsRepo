1170833910b2091944e6d3bcaa69e529b67af105
DaniPlancha <104681854+DaniPlancha@users.noreply.github.com>
2022-05-23 22:19:16 +0300

    readme and js file updated

--- Front end/JS button clicker/script.js
+++ Front end/JS button clicker/script.js
@@ -20,3 +20,35 @@ function Add1 () {
         temp = 0;
     }
 }
+
+let a = (money) => {
+    return new Promise((resolve, reject) => {
+        if (money >= 100000) {
+            resolve("You got a shit ton of money! Rich mf");
+        } else if (money >= 10000) {
+            resolve("You got allot of cash");
+        } else {
+            reject("You dont got enough");
+        }
+    });
+}
+
+async function doWork (money) {
+    try {
+        console.log("we are in the async function now. next line is await");
+        let message = await a(money);
+        console.log(message);
+        setTimeout(() => {
+            console.log("testing timeout");
+        }, 2000);
+    } catch (error) {
+        console.log("Console's err message: " + error);
+    }
+}
+
+doWork(1000000);
+console.log("test async");
+console.log("test async2");
+console.log("test async3");
+
+a(100).then()
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Hello there!
 Personal projects, games, etc.
-Im a Junior Software Developer who is also an intern and still in school and this repository is for all of my projects (C#, SQL, HTML, CSS, JS{in the future}).
+Im a Junior Software Developer who is also an intern and still in school and this repository is for all of my projects (C#, MySQL, HTML, CSS, JavaScript).
